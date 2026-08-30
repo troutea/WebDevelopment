12a7da29af0af3af8063976a790a94b9cbe2b461
Anthony Trout <troutea@users.noreply.github.com>
2026-08-30 15:38:22 +0100

    initial checkin

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+  
+   //myelement
+
+    <StrictMode>
+      <h1>Hello React!</h1>
+     <App />
+    </StrictMode> 
+  
+)
+
+
+ 
