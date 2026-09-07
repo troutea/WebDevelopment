40ccaeb672dccfbc10e68760d5c98f99473cd172
Anthony <troutea@yahoo.co.uk>
2026-09-07 10:03:42 +0100

    initial checkin of quizgame tests

---
+++ Quiz_Game/test/tests/QuizGame.spec.js
@@ -0,0 +1,23 @@
+
+
+const {test} = require('@playright/test');
+
+
+
+test('QuizGameTest_001', async ({browser,page})=>
+{
+     chrome - plugins/ cookies
+   const context =  await browser.newContext();
+   const page = await context.newPage();
+   await page.goto("http://127.0.0.1:5502/index.html");
+
+
+});
+
+test('QuizGameTest_002', async ({page})=>
+{
+   
+   await page.goto("http://127.0.0.1:5502/index.html");
+
+
+});
