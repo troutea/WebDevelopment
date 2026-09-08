320aaaca9e8d8d6abda1343f815a345b89efb99a
Anthony <troutea@yahoo.co.uk>
2026-09-08 12:12:18 +0100

    adding folder for ease of management

---
+++ 100Projects/Quiz_Game/test/tests/QuizGame.spec.js
@@ -0,0 +1,28 @@
+
+
+const {test, expect} = require('@playwright/test');
+//import { test, expect } from '@playwright/test';
+//const {expect} = require('../playwright.config');
+
+
+test('QuizGameTest_001', async ({ page }) => {
+
+   //   chrome - plugins/ cookies
+   // const context =  await browser.newContext();
+   // const page = await context.newPage();
+   await page.goto("https://google.com");
+   console.log(await page.title());
+   await expect(page).toHaveTitle("Google");
+
+});
+
+test('QuizGameTest_002', async ({page})=>
+{
+   
+   await page.goto("http://127.0.0.1:5502/index.html");
+   // get title 
+   console.log(await page.title());
+   await  expect(page).toHaveTitle("Quiz Game")
+
+
+});
