618820835c4ed2af2384ffc631d48e16b6220295
Anthony <troutea@yahoo.co.uk>
2026-09-21 16:04:11 +0100

    adding files

---
+++ 100Projects/ColorPalette/script.js
@@ -0,0 +1,9 @@
+
+const generateBtn = document.getElementById(generate-btn);
+const paletteContainer = document.querySelector(".palette-container");
+
+generateBtn.addEventListener("click", generatePalette);
+
+function generatePalette() {
+    
+}
